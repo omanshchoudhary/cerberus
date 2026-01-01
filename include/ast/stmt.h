706c70da56c68e84e4bc9455ee34768664f6dded
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-01-01 13:19:54 +0530

    Add statement AST nodes for let and print

---
+++ include/ast/stmt.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<string>
+#include "expr.h"
+
+struct Stmt {
+    virtual ~Stmt() = default;
+};
+
+struct LetStmt : Stmt {
+    std::string name;
+    Expr* initializer;
+
+    LetStmt(const std::string& name, Expr* initializer) : name(name), initializer(initializer) {}
+};
+
+struct PrintStmt : Stmt {
+    Expr* expression;
+    
+    explicit PrintStmt(Expr* expression) : expression(expression);
+};
