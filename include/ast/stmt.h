e8eefbba706c8b4c024a2716ee63fadcf2255d2d
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-06-12 01:08:17 +0530

    Add IfStmt and WhileStmt statement nodes

--- include/ast/stmt.h
+++ include/ast/stmt.h
@@ -1,20 +1,42 @@
 #pragma once
 #include<string>
+#include<vector>
 #include "expr.h"
 
-struct Stmt {
+struct Stmt
+{
     virtual ~Stmt() = default;
 };
 
-struct LetStmt : Stmt {
+struct LetStmt : Stmt
+{
     std::string name;
-    Expr* initializer;
+    Expr *initializer;
 
-    LetStmt(const std::string& name, Expr* initializer) : name(name), initializer(initializer) {}
+    LetStmt(const std::string &name, Expr *initializer) : name(name), initializer(initializer) {}
 };
 
-struct PrintStmt : Stmt {
-    Expr* expression;
-    
-    explicit PrintStmt(Expr* expression) : expression(expression){}
+struct PrintStmt : Stmt
+{
+    Expr *expression;
+
+    explicit PrintStmt(Expr *expression) : expression(expression) {}
+};
+
+struct BlockStmt : Stmt
+{
+    std::vector<Stmt *> statements;
+};
+
+struct IfStmt : Stmt
+{
+    Expr *condition;
+    Stmt *thenBranch;
+    Stmt *elseBranch;
+};
+
+struct WhileStmt : Stmt
+{
+    Expr *condition;
+    Stmt *body;
 };
